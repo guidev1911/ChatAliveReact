1d7b5f70ae1e5764a197f9b9c6bb6cc793d6e8f8
guidev1911 <guilhermereal1911@gmail.com>
2025-07-14 23:53:31 -0300

    get foto de perfil

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -64,7 +64,7 @@ export default function Main() {
         {/* Foto de perfil */}
         <div className="w-40 h-40 rounded-full overflow-hidden shadow-md mb-4">
           <img
-            src={`${backendUrl}${user.photoUrl}`} // <-- Aqui usamos a URL montada corretamente
+            src={`${backendUrl}${user.photoUrl}`} 
             alt="Foto do usuário"
             className="w-full h-full object-cover"
           />
